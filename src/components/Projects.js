59978535c72f5ef93255017ff87e96a0c01aa863
Marvin Henriquez <marvinshb@gmail.com>
2022-09-19 15:59:00 -0500

    new profile picture

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,11 @@ import { useEffect } from 'react';
 
 
 function Projects() {
-  const proyectos = [{ link: "https://climappte.netlify.app/", name: "climappte", descripcion: "descripcion del proyecto", }]
+  const proyectos = [
+    { link: "https://climappte.netlify.app/", name: "climappte", descripcion: "web del clima donde puedes informarte acerca del clima en cualquier ciudad que desees ", },
+    { link: "https://marfilms.netlify.app/", name: "marfilms", descripcion: "Pagina web donde se muestra un listado de las ultimas peliculas ", },
+    { link: "https://climappte.netlify.app/", name: "climappte", descripcion: "descripcion del proyecto", }
+  ]
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, [])
