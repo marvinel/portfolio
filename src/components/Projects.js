000bd7f22ef34276b1cacc91bc1476aa4bc76e16
Marvin Henriquez <marvinshb@gmail.com>
2022-09-19 17:05:55 -0500

    optimized images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,9 +6,9 @@ import { useEffect } from 'react';
 
 function Projects() {
   const proyectos = [
-    { link: "https://climappte.netlify.app/", name: "climappte", descripcion: "web del clima donde puedes informarte acerca del clima en cualquier ciudad que desees ", },
-    { link: "https://marfilms.netlify.app/", name: "marfilms", descripcion: "Pagina web donde se muestra un listado de las ultimas peliculas ", },
-    { link: "https://climappte.netlify.app/", name: "climappte", descripcion: "descripcion del proyecto", }
+    { link: "https://climappte.netlify.app/", name: "Climappte", descripcion: "web del clima donde puedes informarte acerca del clima en cualquier ciudad que desees ", },
+    { link: "https://marfilms.netlify.app/", name: "Marfilms", descripcion: "Pagina web donde se muestra un listado de las ultimas peliculas ", },
+    { link: "https://climappte.netlify.app/", name: "Quinterest", descripcion: "descripcion del proyecto", }
   ]
   useEffect(() => {
     Aos.init({ duration: 2000 });
