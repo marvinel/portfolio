2b195294474f94b85187d558424410a4726e2751
Marvin Henriquez <marvinshb@gmail.com>
2022-10-20 20:30:26 -0500

    testing pdf

--- src/components/pdf.js
+++ src/components/pdf.js
@@ -3,6 +3,7 @@
 function Pdf() {
     return (
         <div style={{ position: 'absolute', width: '100%', height: '100%' }} >
+            <h1>Hola</h1>
             <object
                 data={require('../assets/documento/Ecv.pdf')}
                 type="application/pdf"
